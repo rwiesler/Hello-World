1309d71beefce96e87b736b6a69b8f92649f88c5
Ralph Wiesler <github@wira-net.de>
2011-06-08 22:11:30 +0200

    Added a bunch of 'HelloWorld' programming language examples

---
+++ src/HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ 
+ main(void)
+ {
+    printf("Hello, world!\n");
+    return 0;
+ }
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Hello World example in Java.
+ */
+public class HelloWorld{
+
+public static void main(String[] args){
+
+System.out.println("Hello World!");
+
+}
+}
